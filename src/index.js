040d52d1dfee201e696381832862d3953756b665
Bartosz Oleaczek <bartek@localhost.localdomain>
2018-07-25 11:33:01 +0200

    added loading icon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-
-"use strict";
+import ReactSVG from "react-svg"
 
 class CommentsList extends React.Component {
 
@@ -11,38 +10,60 @@ class CommentsList extends React.Component {
 
         this.state = {
           comments: [],
-          howManyLoaded: 0
+          howManyLoaded: 0,
+          noMoreLeft: false,
+          waitingForResponse: false
         }
 
         this.getComments = this.getComments.bind(this);
     }
 
     render() {
+        let button;
+        if(this.state.waitingForResponse) {
+            button = <ReactSVG className="loadingIcon" path="./assets/load_icon.svg" />;
+        }
+        else if(this.state.noMoreLeft) {
+            button = null;
+        }
+        else {
+            button = <button className="get_more_btn" onClick={() => this.getComments()}>Load comments</button>;
+        }
+        
         return(
             <div className="root">
                 <ul className="comments_list">
                     {this.state.comments}
                 </ul>
-                <button className="get_more_btn" onClick={() => this.getComments()}>Load comments</button>
+                {button}
             </div>
         );
     }
 
     async getComments() {
+        this.setState({
+            waitingForResponse:true
+        })
         let response = await axios.get(this.props.url + `?loaded=${this.state.howManyLoaded}`);
         
+        console.log(response.data);
+
         let loaded = this.state.comments;
-        for(let comment of response.data)
+        for(let comment of response.data.comments)
         {
+            console.log(comment);
             loaded.push(<Comment 
-                authorName={comment.authorName}
+                authorName={comment.author}
                 publicatinDate={comment.publicationDate}
-                commentContent={comment.commentContent} />);
+                commentContent={comment.content} 
+                key={loaded.length + 1}/>);
         }
 
         this.setState({
             comments:loaded,
-            howManyLoaded: loaded.length
+            howManyLoaded: loaded.length,
+            noMoreLeft: response.data.isLast,
+            waitingForResponse: false
         })
     }
 }
@@ -50,7 +71,7 @@ class CommentsList extends React.Component {
 class Comment extends React.Component {
     render() {
         return(
-            <li className="comment">
+            <li className="comment" key={this.props.key}>
                 <p className="authors_name">{this.props.authorName}</p>
                 <p className="publication_date">{this.props.publicatinDate}</p>
                 <div className="commentContent" dangerouslySetInnerHTML={{__html: this.props.commentContent}}>
@@ -60,4 +81,8 @@ class Comment extends React.Component {
     }
 }
 
-ReactDOM.render(<CommentsList url="http://127.0.0.1:5000/getc"/>, document.getElementById("index"));
+
+let comments_app = document.getElementById("comments_app")
+
+ReactDOM.render(<CommentsList 
+    url={comments_app.dataset.url} />, comments_app);
